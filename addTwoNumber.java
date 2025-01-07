d017af4e1f329319480c8317239eafa12bfa9827
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-01-08 00:17:20 +0530

    solved addToNumber

---
+++ addTwoNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class addTwoNumber{
+
+    public static void main(String[] args) {
+	Scanner myObj = new Scanner(System.in);
+	int a = myObj.nextInt();
+	int b = myObj.nextInt();
+        System.out.println(a+b);
+    } 
+}
